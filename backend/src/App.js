ad9975b94faa017d093abafd235140a935d0f1d9
Gerson Mayer <g.mayerduran@gmail.com>
2021-04-19 16:26:53 -0500

    Frontend success on fetching

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// const [isError, setIsError] = useState(false);
+	// const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState({});
+	useEffect(() => {
+		async function fetchData() {
+			const data = await axios("/api/todos/");
+			console.log(...data.data);
+			setData(...data.data);
+		}
+		fetchData();
+	}, []);
+
+	return (
+		<div className="container">
+			<div className="columns">
+				<div className="column is-one-third"></div>
+				<div className="column is-one-third">
+					<h1>{data.title}</h1>
+					<h1>{data.description}</h1>
+					<h1>
+						¿La tarea esta completada?
+						{data.completed ? <h2>Si</h2> : <h2>No</h2>}
+					</h1>
+				</div>
+				<div className="column is-one-third"></div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
